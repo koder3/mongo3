104a3e53d81e48432f62a52a428dfc0424b01a05
koder3 <zamir160@yahoo.com>
2018-09-09 16:33:55 +0400

    user addedd

--- server/server.js
+++ server/server.js
@@ -80,10 +80,7 @@ app.patch('/todos/:id', (req, res) => {
 
 app.post('./users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password'])
-    var user = new User({
-        email: body.email,
-        password: body.password
-    })
+    var user = new User(body)
     user.save().then((doc) => {
         res.send(doc)
     },
