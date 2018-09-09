50262963f6e5b926655cb4b7d43408318d54dbfd
koder3 <zamir160@yahoo.com>
2018-09-09 16:45:03 +0400

    user addeddd

--- server/server.js
+++ server/server.js
@@ -82,7 +82,7 @@ app.post('./users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password'])
     var user = new User(body)
     user.save().then((doc) => {
-        res.send(doc)
+        res.status(200).send(doc)
     },
      (e) => {
          res.status(400).send(e)
